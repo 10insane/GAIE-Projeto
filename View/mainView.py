10948c255b771285ca9604fa536990a114c7b120
10insane <sidnei.daniel.goncalves@gmail.com>
2025-10-21 16:21:03 +0100

    teste2

--- View/mainView.py
+++ View/mainView.py
@@ -61,7 +61,6 @@ def main(page: ft.Page):
                 ft.ElevatedButton("Inserir Técnico", icon=ft.Icons.PERSON_ADD, on_click=abrir_modal),
                 ft.ElevatedButton("Inserir estado processo", icon=ft.Icons.PERSON_ADD, on_click=abrir_modal),
                 ft.ElevatedButton("Inserir problemáticaSPO", icon=ft.Icons.PERSON_ADD, on_click=abrir_modal),
-                ft.ElevatedButton("ola", icon=ft.Icons.PERSON_ADD, on_click=abrir_modal)
                 
             ],
             alignment=ft.MainAxisAlignment.SPACE_BETWEEN
